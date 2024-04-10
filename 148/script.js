4ac8d1ab4105e324ca2fdc9d81b5a7e2845cf1f9
drew <d.rewgallinger@gmail.com>
2024-04-10 01:17:23 -0400

    feature: split a1 into code and documentation, added surveys

--- 148/script.js
+++ 148/script.js
@@ -1,6 +1,7 @@
 function loadSavedGrades() {
   const elements = {
     a1: document.getElementById("a1"),
+    a1Documentation: document.getElementById("a1Documentation"), 
     a2: document.getElementById("a2"),
     labs: document.getElementById("labs"),
     preps: document.getElementById("preps"),
@@ -9,16 +10,29 @@ function loadSavedGrades() {
     final: document.getElementById("final"),
   };
 
+  const surveyElements = {
+    survey1: document.getElementById("survey1"),
+    survey2: document.getElementById("survey2"),
+    survey3: document.getElementById("survey3"),
+  }
+
   const grades = JSON.parse(window.localStorage.getItem("grades"));
+  const surveyGrades = JSON.parse(window.localStorage.getItem("surveyGrades"));
 
   for (let key in elements) {
     elements[key].value = grades[key];
   }
+
+  for (let key in surveyElements) {
+    surveyElements[key].checked = surveyGrades[key];
+  }
 }
+
 
 function calculate() {
   const grades = {
     a1: Number(document.getElementById("a1").value),
+    a1Documentation: Number(document.getElementById("a1Documentation").value),
     a2: Number(document.getElementById("a2").value),
     labs: Number(document.getElementById("labs").value),
     preps: Number(document.getElementById("preps").value),
@@ -27,8 +41,15 @@ function calculate() {
     final: Number(document.getElementById("final").value),
   };
 
+  const surveyElements = {
+    survey1: document.getElementById("survey1").checked,
+    survey2: document.getElementById("survey2").checked,
+    survey3: document.getElementById("survey3").checked,
+  };
+
   const weightings = {
-    a1: 15,
+    a1: 10,
+    a1Documentation: 5,
     a2: 10,
     labs: 8,
     preps: 7,
@@ -38,18 +59,26 @@ function calculate() {
   };
 
   window.localStorage.setItem("grades", JSON.stringify(grades));
+  window.localStorage.setItem("surveyGrades", JSON.stringify(surveyElements));
 
   let weightedSum = 0;
   for (let key in grades) {
     weightedSum += grades[key] * weightings[key];
   }
 
-  let result =
-    Math.round((weightedSum * 100.0) / sumObjValues(weightings)) / 100;
+  let result = Math.round((weightedSum * 100.0) / sumObjValues(weightings)) / 100;
+  for (let key in surveyElements) {
+    if (surveyElements[key]) {
+      result += 0.5;
+    }
+  }
+  if (surveyElements.survey1 && surveyElements.survey2 && surveyElements.survey3) {
+    result += 0.5;
+  }
   let displayResult = document.getElementById("result");
-  let displayImg;
 
   // choose which image to show
+  let displayImg;
   if (result < 80) {
     displayImg = "wtf.webp";
   } else {
@@ -58,6 +87,7 @@ function calculate() {
 
   displayResult.innerHTML = `your grade is: ${result}% <img id="result-img" style="vertical-align:middle" src="/assets/${displayImg}" />`;
 }
+
 
 function sumObjValues(obj) {
   let sum = 0;
